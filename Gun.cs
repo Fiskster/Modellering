cb471764fc945d0dce50cc33539f2c1afbf82e9e
Axel <axel.lilja@elev.ga.ntig.se>
2020-09-09 10:06:52 +0200

    gitcommit functional

---
+++ Gun.cs
@@ -0,0 +1,11 @@
+using System;
+
+public class Gun 
+{
+   public float x;
+
+    public float y;
+
+    public float z;
+
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Modellering
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Gun firearm = new Gun();
+            Gun pistol = new Gun();
+
+            firearm.x = 42;
+
+            pistol.x = 22;
+            
+
+        }
+    }
+}
